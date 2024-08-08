168d6783142c33022028266f659c45b6a26dba31
kxznn <kauaroaguiar16@gmail.com>
2024-08-08 10:02:23 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,8 +2,10 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
+import streamlit as kxzz
+from streamlit import title
 
 st.title("Olá, Kaua e Murilo")
 st.header( 'Turma 2EI-SESI' )
 st.subheader( 'sesi - senai' )
-st.subheader( 'NOIS É ZIKA E O RESTO É BURACO!!')
+st.subheader( 'aqui é 00 não 01 e nem 02')
