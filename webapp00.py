c7b957d07fb659d04a2c5cf6babda49c31443ce3
kxznn <kauaroaguiar16@gmail.com>
2024-08-08 09:51:16 -0300

    Update webapp00.py
    att do texto

--- webapp00.py
+++ webapp00.py
@@ -3,4 +3,6 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-st.title("Olá, Massaki!")
+st.title("Olá, Kaua e Murilo")
+st.header( 'Turma 2EI-SESI' )
+st.subheader( 'NOIS É ZIKA E O RESTO É BURACO!!' )
