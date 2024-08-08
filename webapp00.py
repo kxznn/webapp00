d9e31416851ec47a6cdf378d3159bf061ed33986
kxznn <kauaroaguiar16@gmail.com>
2024-08-08 10:03:04 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -5,7 +5,7 @@ import streamlit as st
 import streamlit as kxzz
 from streamlit import title
 
-st.title("Olá, Kaua e Murilo")
-st.header( 'Turma 2EI-SESI' )
-st.subheader( 'sesi - senai' )
-st.subheader( 'aqui é 00 não 01 e nem 02')
+title("Olá, Kaua e Murilo")
+header( 'Turma 2EI-SESI' )
+subheader( 'sesi - senai' )
+subheader( 'aqui é 00 não 01 e nem 02')
